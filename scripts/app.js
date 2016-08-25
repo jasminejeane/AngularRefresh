1f413a877516c811da41efad351474f578350ff3
jasminejeane <jasminejeane@gmail.com>
2016-08-25 16:00:17 -0700

    progress commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,25 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+.controller('mainCtrl', function($scope, dataService){
+
+$scope.learningNgChange = function(){
+
+console.log("An input changed");
+};
+
+$scope.helloConsole = dataService.helloConsole;
+$scope.todos = dataService.getTodos;
+
+})
+.service('dataService', function($http){
+  this.helloConsole = function(){
+    console.log("This is a hello console service");
+  };
+
+  this.getTodos = 
+   $http.get('mock/todos.json')
+  .then(function(response){
+    console.log("This is response data", response.data);
+    return response.data;
+  });
+
+});
