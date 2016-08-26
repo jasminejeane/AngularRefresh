e9dff58712f0bd541b42a2195e118d049fa3af8e
jasminejeane <jasminejeane@gmail.com>
2016-08-25 19:23:00 -0700

    completed todo

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,2 @@
-angular.module("todoListApp", [])
-.controller('mainCtrl', function($scope, dataService){
+angular.module("todoListApp", []);
 
-$scope.learningNgChange = function(){
-
-console.log("An input changed");
-};
-
-$scope.helloConsole = dataService.helloConsole;
-
-dataService.getTodos(function(response){
-  console.log(response.data);
-  $scope.todos = response.data;
-});
-
-})
-.service('dataService', function($http){
-  this.helloConsole = function(){
-    console.log("This is a hello console service");
-  };
-
-  this.getTodos = function(callback){
-   $http.get('mock/todos.json')
-
-  .then(callback);
-    };
-
-});
